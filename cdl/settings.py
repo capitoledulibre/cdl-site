e3c3827ef4beb67e5c6c5eeceb88d6d603f93639
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2015-06-26 23:42:39 +0200

    Translations for proposals

--- cdl/settings.py
+++ cdl/settings.py
@@ -26,7 +26,7 @@ ALLOWED_HOSTS = []
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = "US/Eastern"
+TIME_ZONE = "Europe/Paris"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
