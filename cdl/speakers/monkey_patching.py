bb101a9730a20456edba14dfbc84c7150d351928
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2015-07-02 20:48:34 +0200

    Fix default on speaker boolean fields

--- cdl/speakers/monkey_patching.py
+++ cdl/speakers/monkey_patching.py
@@ -6,5 +6,5 @@ from symposion.speakers.models import Speaker
 
 Speaker.add_to_class('organisation', models.CharField(max_length=100,blank=True))
 Speaker.add_to_class('city', models.CharField(max_length=255,blank=True))
-Speaker.add_to_class('need_travel', models.BooleanField())
-Speaker.add_to_class('need_hosting', models.BooleanField())
+Speaker.add_to_class('need_travel', models.BooleanField(default=False))
+Speaker.add_to_class('need_hosting', models.BooleanField(default=False))
